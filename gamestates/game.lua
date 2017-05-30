09fd103101575021006447374b06359a02185982
rilifon <pudimdefrutas@gmail.com>
2017-05-30 18:11:03 -0300

    Added STEAMING template
    reference: https://github.com/uspgamedev/STEAMING

---
+++ gamestates/game.lua
@@ -0,0 +1,33 @@
+--MODULE FOR THE GAMESTATE: GAME--
+
+local state = {}
+
+function state:enter()
+
+end
+
+function state:leave()
+
+end
+
+
+function state:update(dt)
+
+	Util.destroyAll()
+
+end
+
+function state:draw()
+
+    Draw.allTables()
+
+end
+
+function state:keypressed(key)
+
+    Util.defaultKeyPressed(key)
+
+end
+
+--Return state functions
+return state
